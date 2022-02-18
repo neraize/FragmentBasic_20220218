2613438fb41b891c32266f633c9ffed67759bf49
seojungmin <neraize@gmail.com>
2022-02-18 15:56:07 +0900

    두번째 프래그먼트에서 토스트 띄우기  -this대신 requireContext()로 대체하자

--- app/src/main/java/com/nepplus/fragmentbasic_20220218/fragments/MySecondFragment.kt
+++ app/src/main/java/com/nepplus/fragmentbasic_20220218/fragments/MySecondFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.nepplus.fragmentbasic_20220218.R
+import kotlinx.android.synthetic.main.fragment_my_second.*
 
 class MySecondFragment:Fragment() {
 
+    // 1. 어떤 xml? -onCreateView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -18,5 +21,13 @@ class MySecondFragment:Fragment() {
         return inflater.inflate(R.layout.fragment_my_second, container, false)
     }
 
-    // 2. 어떤동작?  - onActivityCreated
+
+    //2. 어떤 동작?  -onActivityCreated
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnToast.setOnClickListener {
+            Toast.makeText(requireContext(), "프래그먼트에서 띄우는 토스트", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
